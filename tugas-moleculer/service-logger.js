6c85e587ca61a83851721f885895085d9b99e207
Erlangga15 <55235710+Erlangga15@users.noreply.github.com>
2021-02-24 16:38:16 +0700

    add service-logger

---
+++ tugas-moleculer/service-logger.js
@@ -0,0 +1,34 @@
+const { ServiceBroker } = require("moleculer");
+const DbService = require("moleculer-db");
+
+const nodeLogger = new ServiceBroker({
+  namespace: "dev",
+  nodeID: "node-logger",
+  transporter: "NATS",
+});
+
+nodeLogger.createService({
+  name: "logger",
+  mixins: [DbService],
+
+  settings: {
+    fields: ["_id", "action", "date"],
+  },
+
+  actions: {
+    listLogger: {
+      async handler(ctx) {
+        return this.broker.call("logger.find", {});
+      },
+    },
+    createLogger: {
+      async handler(ctx) {
+        return this.broker.call("logger.create", ctx.params);
+      },
+    },
+  },
+});
+
+Promise.all([nodeLogger.start()]).then(() => {
+  nodeLogger.repl();
+});
