b96c3cf5ca48e1658a78c2339ac3787cfc5e3ca9
Inosensius Karelo Hesay <inosensius241099@gmail.com>
2021-02-24 21:19:42 +0700

    finished logger

--- assignment/logger.js
+++ assignment/logger.js
@@ -1,9 +1,46 @@
 const { ServiceBroker } = require("moleculer");
-const HTTPServer = require("moleculer-web");
 const DbService = require("moleculer-db");
 
-const brokerNode1 = new ServiceBroker({
+const brokerLogger = new ServiceBroker({
     namespace: "dev",
     nodeID: "logger-gateway",
     transporter: "NATS",
   });
+
+  brokerLogger.createService({
+    name: "loggers",
+    mixins: [DbService],
+
+    settings: {
+        fields: ["_id","action", "date"],
+        entityValidator: {
+          action: 'string',
+          date: 'string',
+        }
+    },
+
+    actions: {
+      listLog: {
+        async handler(ctx){
+            return await this.broker.call("loggers.find",{}).then((res)=>{
+                return res
+            }).catch((err)=>{
+                console.log(err);
+            })
+        }
+      },
+        createLog: {
+            async handler(ctx) {
+                return await this.broker.call("loggers.create", ctx.params).then((res) => {
+                    return res;
+                }).catch((err)=>{
+                    console.log(err);
+                })
+            }
+        },
+    }
+})
+
+Promise.all([brokerLogger.start()]).then(()=>{
+  brokerLogger.repl()
+}) 
