f5f7d04ad9c5ef31dac8469d9ba48d85c8d28aa1
Muhammad Choirudin Malik <77602202+choirmalik@users.noreply.github.com>
2021-02-24 12:02:28 +0700

    logger done

--- tugas/logger.js
+++ tugas/logger.js
@@ -1,1 +1,39 @@
-.gitkeep
+const { ServiceBroker } = require("moleculer");
+const DbService = require("moleculer-db");
+
+const logBroker = new ServiceBroker({
+	namespace: "dev",
+  nodeID: "logger-node",
+  transporter: "NATS"
+});
+
+logBroker.createService({
+  name: "logger",
+  mixins: [DbService],
+
+  settings: {
+    fields: ["_id", "action", "date"],
+		entityValidator: {
+			action: "string"
+		}
+   },
+
+  actions: {
+    listLogs: {
+    	async handler(ctx) {
+    		return this.broker.call("logger.find", {});
+    	}
+    },
+    createLog: {
+    	async handler(ctx) {
+				const datetime = new Date();
+				const parameters = {action: ctx.params.action, date: datetime};
+    		return this.broker.call("logger.create", parameters);
+    	}
+    }
+  },
+});
+
+Promise.all([logBroker.start()]).then(() => {
+  logBroker.repl();
+});
