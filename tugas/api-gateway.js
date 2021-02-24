743921af7febe656442498e92b1f9f9723fed193
Muhammad Achsan Hujjatul Islam <achsanh@gmail.com>
2021-02-24 04:52:52 -0500

    [Checkpoint 1] - Simple CRUD with base case done

--- tugas/api-gateway.js
+++ tugas/api-gateway.js
@@ -4,13 +4,12 @@ const HTTPServer = require("moleculer-web");
 const brokerNode1 = new ServiceBroker({
   namespace: "dev",
   nodeID: "node-1",
-  transporter: "NATS"
+  transporter: "NATS",
 });
-
 
 brokerNode1.createService({
   name: "gateway",
-  mergeParams:false,
+  mergeParams: false,
   mixins: [HTTPServer],
 
   settings: {
@@ -20,66 +19,67 @@ brokerNode1.createService({
     routes: [
       {
         path: "/api",
-        aliases: {      
-          "GET users"(req, res){
-            this.broker.call("users.listUsers", {})
-            .then((response) => {
+        aliases: {
+          "GET users"(req, res) {
+            this.broker.call("users.listUsers", {}).then((response) => {
               res.end(JSON.stringify(response));
             });
           },
-          "POST users"(req, res){
-            this.broker.call("users.createUsers", {})
-            .then((response) => {
+          "POST users"(req, res) {
+            this.broker.call("users.createUser", req.body).then((response) => {
               res.end(JSON.stringify(response));
             });
           },
-          "PUT users"(req, res){
-            this.broker.call("users.updateUsers", {})
-            .then((response) => {
+          "PUT users"(req, res) {
+            this.broker.call("users.updateUser", req.body).then((response) => {
               res.end(JSON.stringify(response));
             });
           },
-          "GET transactions"(req, res){
-            this.broker.call("transactions.listTransaction", {})
-            .then((response) => {
+          "DELETE users"(req, res) {
+            this.broker.call("users.deleteUser", {}).then((response) => {
               res.end(JSON.stringify(response));
             });
           },
-          "POST transactions"(req, res){
-            this.broker.call("transactions.createTransaction", {})
-            .then((response) => {
+          "GET transactions"(req, res) {
+            this.broker
+              .call("transactions.listTransaction", {})
+              .then((response) => {
+                res.end(JSON.stringify(response));
+              });
+          },
+          "POST transactions"(req, res) {
+            this.broker
+              .call("transactions.createTransaction", {})
+              .then((response) => {
+                res.end(JSON.stringify(response));
+              });
+          },
+          "GET log"(req, res) {
+            this.broker.call("log.listLog", {}).then((response) => {
               res.end(JSON.stringify(response));
             });
           },
-          "GET log"(req, res){
-            this.broker.call("log.listLog", {})
-            .then((response) => {
-              res.end(JSON.stringify(response));
-            });
-          }
-
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
 
   actions: {
     home: {
-        async handler() {
-            return "My Home";
-        }
+      async handler() {
+        return "My Home";
+      },
     },
     welcome: {
-        params: {
-            name: "string"
-        },
-        async handler(ctx) {
-            return `Welcome, ${ctx.params.name}`;
-        }
-    }
-  }
+      params: {
+        name: "string",
+      },
+      async handler(ctx) {
+        return `Welcome, ${ctx.params.name}`;
+      },
+    },
+  },
 }); //batas api
-
 
 // Start brokers
 Promise.all([brokerNode1.start()]).then(() => {
---
+++ tugas/user.js
@@ -0,0 +1,83 @@
+const { ServiceBroker } = require("moleculer");
+const DbService = require("moleculer-db");
+
+const brokerNode3 = new ServiceBroker({
+  namespace: "dev",
+  nodeID: "node-3",
+  transporter: "NATS",
+});
+
+var users;
+var userID = 1;
+var user = {};
+
+brokerNode3.createService({
+  name: "users",
+  mixins: [DbService],
+
+  settings: {
+    fields: ["_id", "name", "email", "address"],
+    entityValidator: {
+      name: "string",
+    },
+  },
+
+  actions: {
+    listUsers: {
+      async handler(ctx) {
+        return this.broker.call("users.find", {});
+      },
+    },
+    createUser: {
+      async handler(ctx) {
+        this.broker.call("users.listUsers", {}).then((response) => {
+          if (response.slice(-1).pop()) {
+            userID = response.slice(-1).pop()["_id"] + 1;
+          }
+
+          user = ctx.params;
+          user["_id"] = userID;
+          this.broker.call("users.create", user);
+        });
+        return "User created successfully";
+      },
+    },
+    updateUser: {
+      async handler(ctx) {
+        const user = await this.adapter.findOne({ _id: ctx.params._id });
+        if (user) {
+          try {
+            this.broker.call("users.update", ctx.params);
+          } catch (error) {
+            return error;
+          }
+          return "User was updated";
+        } else {
+          return "User was not found";
+        }
+      },
+    },
+    deleteUser: {
+      async handler(ctx) {
+        const user = await this.adapter.findOne({ _id: 1 });
+        console.log(user);
+        if (user) {
+          try {
+            await this.adapter.db.remove({ _id: 1 });
+          } catch (error) {
+            return error;
+          }
+          return "User was deleted";
+        } else {
+          return "User was not found";
+        }
+      },
+    },
+  },
+
+  afterConnected() {},
+});
+
+Promise.all([brokerNode3.start()]).then(() => {
+  brokerNode3.repl();
+});
