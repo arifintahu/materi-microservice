d6b7bead3d59481c6bd08568d6444d47caf63087
babdurrahman78 <babdurrahman78@gmail.com>
2021-02-24 16:20:43 +0700

    feat : service-user

--- tugas-moleculer/service-transaction.js
+++
@@ -1,8 +0,0 @@
-const { ServiceBroker } = require('moleculer');
-const DbService = require('moleculer-db');
-
-const nodeUser = new ServiceBroker({
-    namespace: 'dev',
-    nodeID: 'node-user',
-    transporter: 'NATS'
-});
